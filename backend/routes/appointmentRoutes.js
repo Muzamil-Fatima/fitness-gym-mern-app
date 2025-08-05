f8eda96e0a787fa8dcc5995b556643aa6fdb10db
Muzamil <muzamilzubair987654@gmail.com>
2025-08-05 23:32:37 +0500

    create appointment

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { createAppointment } from "../controllers/appointmentController.js";
 const router = express.Router();
-export default router;
 
+router.post("/", upload.fields([{ name: "" }]), createAppointment);
+export default router;
